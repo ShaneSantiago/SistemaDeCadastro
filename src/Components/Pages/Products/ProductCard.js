46ff303c0d7838749e9f0654e0674f2efd2a49b7
ShaneSantiago <shane.santiago1@hotmail.com>
2022-04-18 12:14:53 -0300

    atualização

--- src/Components/Pages/Products/ProductCard.js
+++ src/Components/Pages/Products/ProductCard.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Card, Typography, CardContent } from "@mui/material";
-import { convertDate } from "../../Utils/ConvertDate";
+
 
 
 
@@ -23,7 +23,7 @@ const ProductCard = (props) => {
         <Typography >
           Descrição: {props.description}
           <br />
-          Data da publicação: {convertDate(props.date)}
+          Data da publicação: {props.date}
         </Typography>
 
         <Typography color="text.secondary">
